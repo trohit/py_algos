f1fe63436ae5482397e131af276f94a4841c1ead
Rohit <trohit@users.noreply.github.com>
2022-09-05 14:07:05 +0530

    Update mergesort.py

--- mergesort.py
+++ mergesort.py
@@ -3,16 +3,13 @@ import random
 def mergesort(a):
     n = len(a)
     if n <= 1: return
-
     i = j = k = 0
     m = n // 2
-    l = a[:m]
-    r = a[m:]
+    l,r = a[:m], a[m:]
     mergesort(l)
     mergesort(r)
     while i < len(l) and j < len(r):
         if l[i] < r[j]:
-            print(f"a[{k}] = l[{i}] => {l[i]}")
             a[k] = l[i]
             i += 1
         else:
@@ -29,9 +26,7 @@ def mergesort(a):
         k += 1
          
 # main
-# a = [20, 40, 10, 30]
 # a = random.sample(range(100), 10)
 a = [4,3,7,2,9,1,8]
-
 mergesort(a)
 print(f"{a}")
