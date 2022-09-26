c1884d3e2246b9c2e92a8982f279faf21ffff399
Rohit <trohit@users.noreply.github.com>
2022-09-26 19:32:40 +0530

    Update quicksort.py

--- quicksort.py
+++ quicksort.py
@@ -28,10 +28,30 @@ def qs(a):
     a = l + [a[lwi]] + r
     return a
 
+# uses hoare partitioning
+# remember that pivot is also passed as the highest elem 
+def qsh(a):
+    def parth(a, lo, hi):
+        piv = random.choice(a[lo:hi])
+        i, j = lo, hi
+        while True:
+            while a[i] < piv: i+=1
+            while a[j] > piv: j-=1
+            if i >= j: return j
+            a[i], a[j] = a[j], a[i]
+
+    def _qsh(a, lo, hi):
+        if lo < hi:
+            pi = parth(a, lo, hi)
+            _qsh(a, lo, pi)
+            _qsh(a, pi+1, hi)
+
+    _qsh(a, 0, len(a) - 1)
 
 # main
 # import random
 # a = random.sample(range(100), 7)
 a = [4, 3, 7, 2, 9, 1, 8]
 a = qs(a)
+qsh(a)
 print(f"s:{a}")
