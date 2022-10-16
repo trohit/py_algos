0fa222eca1ed9b69fa0ca7bb4e9efabdb6d2b24c
Rohit <trohit@users.noreply.github.com>
2022-10-17 00:17:19 +0530

    Create three_way_partition.py

---
+++ three_way_partition.py
@@ -0,0 +1,26 @@
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        # dutch national flag 3 way partition
+        # O(n)
+        # 0.....i.....j.........k....n
+        #  <mid   mid  unsorted  >mid
+        a = nums # just an alias
+        n = len(nums)
+        i,j,k = 0,0,n-1 # before index, holds the max elm just before pivot
+        while j <= k: # loop until unsorted elms exist
+            print(f"{a} i:{i} j:{j} k:{k}")
+            if a[j] < pivot:
+                print("<lesser")
+                a[j], a[i] = a[i], a[j]
+                i += 1
+                j += 1
+            elif a[j] > pivot:
+                print(">greater")
+                a[j], a[k] = a[k], a[j]
+                k -= 1
+            else: # a[j] == pivot
+                print("equal")
+                j += 1 
+        print(a)
+        return a
+        
